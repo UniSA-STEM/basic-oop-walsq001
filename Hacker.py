2494ae6ed88999bc69dcb196b5b02f1d9b1d5241
Scott Walker <walsq001@mymail.unisa.edu.au>
2025-09-30 14:38:43 +0930

    Change blocked to hidden, introduce trace track function as an attempt at encapsulation

--- Hacker.py
+++ Hacker.py
@@ -13,7 +13,7 @@ import random
 
 crypto_token = 0
 trace_level = 0
-
+max_trace_level = 5
 """
 Hacker class to be imported into main program.
 Each instantiation will pick a random name,
@@ -32,13 +32,28 @@ class Hacker:
         ]
         self.trace_level = trace_level
         self._has_rig = False
-        self._actions_blocked = False
+        self.__blocked = False
 
     def __str__(self):
         return (f"Name: {self.name}\n"
                 f"Inventory: {self.inventory}\n"
                 f"Trace Level: {self.trace_level}")
 
+    def __actions_blocked(self):
+        return self.__blocked == True
+
+    def trace_track(self):
+        if self.trace_level >= max_trace_level:
+            print("Your actions are blocked")
+            return self.__actions_blocked()
+        else:
+            print("Your actions are not blocked")
+            return None
+
 player1 = Hacker()
 player2 = Hacker()
 print(player1)
+player1.trace_track()
+player1.trace_level = 6
+player1.trace_track()
+
